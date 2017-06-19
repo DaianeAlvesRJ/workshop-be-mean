7b11ef18b5932b85bbc3821d6f4f0789d6af2af2
Daiane <dsalves@id.uff.br>
2017-06-19 18:59:16 -0300

    Primeiro Commit

---
+++ nodejs/hello-querystring.js
@@ -0,0 +1,24 @@
+'use strict';
+// file: hello-querystring.js
+let http = require('http')  
+  , url = require('url');
+
+http.createServer(function(request, response){
+
+  var result = url.parse(request.url, true);
+
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write('<html><body>');
+  response.write('<h1>Query string</h1>');
+  response.write('<ul>');
+
+  for(var key in result.query){
+    response.write('<li>'+key+' : '+result.query[key]+'</li>');
+  }
+
+  response.write('</ul>');
+  response.write('</body></html>');
+  response.end();
+}).listen(8080, function(){
+  console.log('Servidor rodando em localhost:3000');
+});
